91bb75d0dba1df16f7ef0b0461f3cffd92fd2fac
rahulsah123 <rahulsah123@users.noreply.github.com>
2018-02-09 14:30:08 +0530

    Update Constants.java

--- app/src/main/java/com/example/rahulkumarsah/loginphp/Constants.java
+++ app/src/main/java/com/example/rahulkumarsah/loginphp/Constants.java
@@ -6,7 +6,7 @@ package com.example.rahulkumarsah.loginphp;
 
 public class Constants {
 
-    private static final String ROOT_URL = "http://172.18.33.118:1234/dashboard/Android/v1/";
+    private static final String ROOT_URL = "http://youripaddress/dashboard/Android/v1/";
 
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
     public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
