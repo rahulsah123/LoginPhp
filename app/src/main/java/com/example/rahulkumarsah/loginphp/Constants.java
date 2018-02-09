69b587680ade01569b4e676eb9b5b349f239e408
Rahul Kumar Sah <smartrahulsah17@gmail.com>
2018-02-09 14:24:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rahulkumarsah/loginphp/Constants.java
@@ -0,0 +1,15 @@
+package com.example.rahulkumarsah.loginphp;
+
+/**
+ * Created by Rahul Kumar Sah on 09-01-2018.
+ */
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://172.18.33.118:1234/dashboard/Android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+
+
+}
